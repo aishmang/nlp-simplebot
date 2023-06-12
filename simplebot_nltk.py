41755e3de8174bd6db086644aa05e9d09d6dcef0
Mangesh Gokhale <mangesh.s.gokhale@accenture.com>
2023-06-12 18:33:48 +0000

    Finished The Simple Bots

--- simplebot_nltk.py
+++ simplebot_nltk.py
@@ -5,9 +5,22 @@ from tkinter import *
 
 
 # Generating response
-def get_bot_response(user_input):
+
    
-  return bot_response
+def get_bot_response(user_input):
+    prompt = f"Please provide a response to the following user input: '{user_input}'"
+
+    response = openai.Completion.create(model="text-davinci-002",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    bot_response = response.choices[0].text.strip()
+    return bot_response
+
 
 
 def create_and_insert_user_frame(user_input):
